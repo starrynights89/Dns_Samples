521ea9272d0cec85752aadc5b5948ed0f26061f9
Alexander Hartson <alexander-hartson@live.com>
2019-10-15 17:22:11 -0500

    first commit

---
+++ Dns_Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dns_Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
